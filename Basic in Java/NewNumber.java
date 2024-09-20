916cfc5689cb44aac4fe81a0c5a940d3d75f7626
Sangram Das <sangramraju143@gmail.com>
2024-09-20 06:26:52 +0530

    new file

---
+++ Basic in Java/NewNumber.java
@@ -0,0 +1,8 @@
+public class NewNumber {
+    public static void main(String[] args) {
+        int num = 10;
+        int num2 = 20;
+        Math.max(num, num2);
+        System.out.println(num2);
+    }
+}
